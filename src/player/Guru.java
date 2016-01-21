8c2aebc5f8a73d70aae4b2435adca637f54ff8bf
SouravD18 <sourav18@mit.edu>
2016-01-21 16:29:05 -0500

    Created Guru

---
+++ src/player/Guru.java
@@ -0,0 +1,17 @@
+package player;
+
+/**
+ * This class is takes account of history, current hand stats and equity of hand.
+ * Then it gives "advice" to our brain
+ * 
+ * @author Sourav
+ *
+ */
+public class Guru {
+    /**
+     * Preflop tactics:
+     *  ->  If high 3-bet% then tighten raising ranges
+     *  ->  Fold-to-4-bet% high compare to 3-bet: 4-bet-bluff a lot
+     *  ->  PFR high? Call and 3-bet often
+     */
+}
